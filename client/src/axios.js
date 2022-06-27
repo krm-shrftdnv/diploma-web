8ea9f36eaca05d1639da17b9ac655ae7087aa749
krm-shrftdnv <shkar2001@mail.ru>
2022-06-27 05:15:34 +0300

    axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: 'http://localhost:3001',
-    baseURL: 'https://offigator-app.herokuapp.com:3001',
+    baseURL: 'https://offigator-app.herokuapp.com' +
+        '',
     headers: {
         "Access-Control-Allow-Origin": "*"
     }
